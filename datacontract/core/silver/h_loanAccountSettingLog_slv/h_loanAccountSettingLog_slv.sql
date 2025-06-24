c6b32abd207dd6aff2bbf0b5fd6ee17c246ce477
CynthiaCarolA <163442771+CynthiaCarolA@users.noreply.github.com>
2025-06-23 21:53:59 -0500

    chore: actualizar datacontract/core/silver/h_loanAccountSettingLog_slv/h_loanAccountSettingLog_slv.sql

--- datacontract/core/silver/h_loanAccountSettingLog_slv/h_loanAccountSettingLog_slv.sql
+++ datacontract/core/silver/h_loanAccountSettingLog_slv/h_loanAccountSettingLog_slv.sql
@@ -13,7 +13,6 @@ CREATE OR REPLACE TABLE core.silver.h_loanAccountSettingLog_slv (
   executionTs TIMESTAMP COMMENT "Fecha y hora de ejecucion de la rutina de carga al datalake en UTC-0." not null
   ,CONSTRAINT h_loanAccountSettingLog_slv_pk PRIMARY KEY(accountSettingId, recordCreationTs, loanAccountSettingTypeCd)
   ,CONSTRAINT h_loanAccountSettingLog_slv_accountSettingId_fk FOREIGN KEY (accountSettingId) REFERENCES core.silver.accountsetting_slv
-  ,CONSTRAINT h_loanAccountSettingLog_slv_loanAccountSettingTypeCd_fk FOREIGN KEY (loanAccountSettingTypeCd) REFERENCES datahub.silver
   ,CONSTRAINT h_loanAccountSettingLog_slv_accountId_fk FOREIGN KEY (accountId) REFERENCES core.silver.m_account_slv
 )
 CLUSTER BY (expirationDt, accountId, accountSettingId, loanAccountSettingTypeCd)
