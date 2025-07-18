9ab590bd10abf17f3651652d89618e9bf4e787ea
CynthiaCarolA <163442771+CynthiaCarolA@users.noreply.github.com>
2025-07-18 11:14:26 -0500

    chore: deploy artifacts from backofficebannersconfigurations_brz

--- datacontract/core/bronze/backofficebannersconfigurations_brz/backofficebannersconfigurations_brz.sql
+++ datacontract/core/bronze/backofficebannersconfigurations_brz/backofficebannersconfigurations_brz.sql
@@ -1,29 +1,29 @@
 -- Data Contract: urn:core.bronze.backofficebannersconfigurations_brz-latest
 -- SQL Dialect: databricks
 CREATE OR REPLACE TABLE core.bronze.backofficebannersconfigurations_brz (
-  end_date STRING COMMENT "Fecha de finalización de la vigencia de un banner, configurada dentro del backoffice. Formato: dd/MM/yyyy HH:mm:ss+00.",
-  owner STRING COMMENT "Owner provider configurado dentro del backoffice, que identifica a qué usuarios se mostrará el banner según el owner provider. Este valor solo se incluye cuando el banner es de tipo MASIVO. Valores posibles: BCP, NIUBIZ, YAPEDIGITAL, YAPEEMPRESA.",
-  product STRING COMMENT "Producto del home banner, que se empleará para los clusters de NBP en el futuro.",
-  __deleted STRING COMMENT "Campo utilizado como convención de integración con Debezium. False indica que el registro no ha sido eliminado (es un registro activo), mientras que True indica que el registro ha sido eliminado (es un 'tombstone').",
-  deeplink STRING COMMENT "Deeplink utilizado en el banner que permite redireccionar a los usuarios al hacer tap.",
-  blacklist STRING COMMENT "Corresponde a un array de valores que indica la lista de segmentos que actuarán como blacklist dentro de la lógica del API Connect Hub. Si un segmento está incluido en esta lista, los usuarios pertenecientes a ese segmento no verán el banner configurado.",
-  description STRING COMMENT "Descripción textual del banner creado, utilizada para funciones de accesibilidad en dispositivos.",
-  created_at STRING COMMENT "Campo que corresponde a la fecha de creación del home banner. Formato: dd/MM/yyyy HH:mm:ss+00.",
-  organization_name STRING COMMENT "Nombre de la ONG u organización utilizada para crear el banner de tipo donación. Este campo solo tendrá valor si el banner es de tipo donación.",
+  end_date STRING COMMENT "Fecha de finalización de la vigencia del banner, configurada en el backoffice. La fecha está en formato UTC-0. Formato: dd/MM/yyyy HH:mm:ssZ.",
+  owner STRING COMMENT "Owner provider configurado en el backoffice, utilizado para identificar a los usuarios a quienes se mostrará el banner según el owner provider. Este valor solo se presenta cuando el banner es de tipo MASIVO. Valores posibles: BCP, NIUBIZ, YAPEDIGITAL, YAPEEMPRESA.",
+  product STRING COMMENT "Producto asociado al home banner, empleado para los clusters de NBP (Next Best Product) en el futuro.",
+  __deleted STRING COMMENT "Campo utilizado como convención de integración con Debezium. False: El registro no ha sido eliminado (registro activo). True: El registro ha sido eliminado (tombstone).",
+  deeplink STRING COMMENT "Deeplink utilizado en el banner para redireccionar a los usuarios cuando interactúan con él.",
+  blacklist STRING COMMENT "Corresponde a un arreglo de valores que indica la lista de segmentos que funcionarán como blacklist dentro de la lógica del API Connect Hub. Si un segmento está incluido en esta lista, los usuarios pertenecientes a ese segmento no visualizarán el banner configurado.",
+  description STRING COMMENT "Descripción textual del banner creado, utilizada para funciones de accesibilidad en los dispositivos.",
+  created_at STRING COMMENT "Fecha de creación del home banner. Formato UTC-0. Formato: dd/MM/yyyy HH:mm:ssZ.",
+  organization_name STRING COMMENT "Nombre de la ONG u organización utilizado para crear el banner de tipo donación. Este campo solo tiene valor si el banner es de tipo donación.",
   title STRING COMMENT "Título asignado al banner dentro del backoffice.",
-  type STRING COMMENT "Indica el tipo de banner. Puede ser de tipo funcional o de tipo donación. Valores esperados: NAVIGATION, DONATION.",
-  confirmation_name STRING COMMENT "Nombre del mensaje de confirmación mostrado en el modal de donación. Este campo solo tendrá valor si el banner es de tipo donación.",
-  banner_type_position STRING COMMENT "Indica el tipo de configuración de segmentación del banner creado. Los valores indican si es de tipo Masivo, Segmentado o NBP. Valores esperados: MASIVO, SEGMENTADO, NBP, DEFAULT.",
-  petitioner STRING COMMENT "Valor que indica el producto al que hace referencia la publicación, configurado dentro del backoffice desde una lista desplegable o un campo de texto.",
+  type STRING COMMENT "Indica el tipo de banner. Puede ser funcional o de donación. Valores posibles: NAVIGATION, DONATION.",
+  confirmation_name STRING COMMENT "Nombre del mensaje de confirmación mostrado en el modal de donación. Este campo solo tiene valor si el banner es de tipo donación.",
+  banner_type_position STRING COMMENT "Indica el tipo de configuración de segmentación del banner. Valores posibles: MASIVO (envíos según owner provider del usuario), SEGMENTADO (segmentación personalizada por bases), NBP (personalización a nivel de usuario), DEFAULT (banners por defecto si el usuario no tiene banners pendientes).",
+  petitioner STRING COMMENT "Valor que indica el producto al que hace referencia la publicación, configurado en el backoffice desde una lista desplegable o campo de texto.",
   url_image STRING COMMENT "URL de la imagen utilizada para mostrar el banner en el carrusel de Yape.",
-  updated_at STRING COMMENT "Campo que corresponde a la fecha de modificación de un registro del banner. Formato: dd/MM/yyyy HH:mm:ss+00.",
-  segment STRING COMMENT "Corresponde a un array de valores que indica el listado de segmentos a los cuales se disponibilizará el banner. Este valor solo se incluye cuando el banner es de tipo SEGMENTADO.",
+  updated_at STRING COMMENT "Fecha de modificación de un registro del banner. Formato UTC-0. Formato: dd/MM/yyyy HH:mm:ssZ.",
+  segment STRING COMMENT "Corresponde a un arreglo de valores que indica el listado de segmentos a los cuales se disponibilizará el banner. Este valor solo se presenta cuando el banner es de tipo SEGMENTADO.",
   id STRING COMMENT "Identificador único del banner creado desde el backoffice, utilizado como clave primaria (PK).",
-  phone_donation STRING COMMENT "Número celular de la ONG a la cual se realizarán los yapeos donativos. Este campo solo tendrá valor si el banner es de tipo donación.",
-  position STRING COMMENT "Indica la posición relativa del banner dentro del carrusel de banners o en un listado de banners en los flujos que lo soporten.",
-  view_scope STRING COMMENT "Indica la pantalla donde deberá aparecer el banner configurado en el backoffice.",
-  start_date STRING COMMENT "Fecha de inicio de la publicación del banner, configurada dentro del backoffice. Formato: dd/MM/yyyy HH:mm:ss+00.",
-  status STRING COMMENT "Indica el estado del registro del banner. Los valores posibles son: BORRADOR (el registro está en borrador y puede ser modificado), VALIDACION (el registro está en validación por un owner), APROBADO (el registro está activado), ELIMINADO (el registro ha sido eliminado desde el backoffice).",
+  phone_donation STRING COMMENT "Número celular de la ONG a la cual se realizarán los yapeos donativos. Este campo solo tiene valor si el banner es de tipo donación.",
+  position STRING COMMENT "Posición relativa del banner dentro del carrusel o listado de banners en los flujos que lo soportan.",
+  view_scope STRING COMMENT "Indica la pantalla donde debe aparecer el banner configurado en el backoffice.",
+  start_date STRING COMMENT "Fecha de inicio de la publicación del banner, configurada en el backoffice. La fecha está en formato UTC-0. Formato: dd/MM/yyyy HH:mm:ssZ.",
+  status STRING COMMENT "Indica el estado del registro del banner. Valores posibles: BORRADOR (el registro aún se encuentra en borrador y puede ser modificado), VALIDACION (el registro está en validación por un owner), APROBADO (el registro está activado), ELIMINADO (el registro ha sido eliminado desde el backoffice).",
   metadata STRUCT<
       partition INT COMMENT "Indica la división lógica y física de un tópico",
       offset BIGINT COMMENT "Indica la posición secuencial de un mensaje dentro de una partición",
@@ -35,7 +35,7 @@ CREATE OR REPLACE TABLE core.bronze.backofficebannersconfigurations_brz (
   p_auditDate DATE COMMENT "Fecha de ejecucion de la rutina de carga al datalake en UTC-0." not null
 )
 CLUSTER BY (auditTime, p_auditDate)
-COMMENT "Esta tabla contiene información sobre los banners configurados en el sistema, incluyendo detalles sobre su vigencia, tipo, propietario y otros atributos relevantes. Cada banner puede ser de tipo funcional o de donación, y su visibilidad puede ser determinada por segmentos específicos. Los campos incluyen fechas de inicio y finalización, así como información sobre la organización asociada en caso de donaciones. Además, se registran detalles como el estado del banner, su posición en el carrusel y la URL de la imagen correspondiente."
+COMMENT "Descripción de la tabla: Esta tabla almacena la configuración y atributos de los banners gestionados desde el backoffice, permitiendo su segmentación, personalización y control de visibilidad en la plataforma. Incluye información relevante como fechas de vigencia, tipo de banner, segmentación, estado, detalles de accesibilidad, y datos específicos para banners de tipo donación, facilitando la administración y despliegue de campañas visuales en la aplicación."
  TBLPROPERTIES (
   'delta.logRetentionDuration' = 'interval 30 days',
   'delta.deletedFileRetentionDuration' = 'interval 30 days',
