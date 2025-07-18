4b27ca8ca28254c6d806013a495354088c377778
CynthiaCarolA <163442771+CynthiaCarolA@users.noreply.github.com>
2025-07-18 00:45:25 -0500

    chore: deploy artifacts from tiendapricessku_brz

--- datacontract/develop/bronze/tiendapricessku_brz/executeddl.sql
+++ datacontract/develop/bronze/tiendapricessku_brz/executeddl.sql
@@ -4,13 +4,13 @@
 -- Data Contract: urn:${env:ENV_CATALOG_NAME_DEVELOP}.bronze.tiendapricessku_brz-latest
 -- SQL Dialect: databricks
 CREATE OR REPLACE TABLE ${env:ENV_CATALOG_NAME_DEVELOP}.bronze.tiendapricessku_brz (
-  itemId STRING COMMENT "Identificador único del SKU. Es un valor incremental autogenerado por la aplicación de VTEX.",
-  listPrice STRING COMMENT "Precio de venta sugerido para el SKU.",
-  costPrice STRING COMMENT "Costo base del SKU, utilizado para calcular márgenes de ganancia.",
-  markup STRING COMMENT "Margen de ganancia deseado para la venta del SKU .",
-  basePrice STRING COMMENT "Precio base o de referencia del SKU, utilizado como punto de partida para cálculos de precios dinámicos o promociones.",
-  fixedPrices STRING COMMENT "Lista de objetos que definen precios fijos para el SKU. Estos precios sobrescriben cualquier otra configuración de precios existente.",
-  updateTs STRING COMMENT "Fecha de actualización de la información del sku. Indica la fecha y hora en formato UTC-5 en que se produjo la actualización del catálogo.",
+  itemId STRING COMMENT "Identificador único del SKU (stock keeping unit). Este valor es autogenerado de manera incremental por la aplicación de VTEX. Ejemplo: 38.",
+  listPrice STRING COMMENT "Precio de venta sugerido para el SKU. Este valor representa el costo que se recomienda al consumidor. Ejemplo: 119.",
+  costPrice STRING COMMENT "Costo base del SKU, utilizado para calcular los márgenes de ganancia. Este valor es fundamental para determinar la rentabilidad del producto. Ejemplo: 49.",
+  markup STRING COMMENT "Margen de ganancia deseado para la venta del SKU. Este valor se expresa en centavos y se utiliza para establecer el precio de venta. Ejemplo: 2040816327.",
+  basePrice STRING COMMENT "Precio base o de referencia del SKU, utilizado como punto de partida para cálculos de precios dinámicos o promociones. Ejemplo: 50.",
+  fixedPrices STRING COMMENT "Lista de objetos que definen precios fijos para el SKU. Estos precios sobrescriben cualquier otra configuración de precios existente, asegurando que se aplique un precio específico. Ejemplo: [] (sin precios fijos).",
+  updateTs STRING COMMENT "Fecha de actualización de la información del SKU. Indica la fecha y hora en formato UTC-5 en que se produjo la actualización del catálogo. Formato: yyyy-MM-dd'T'HH:mm:ss.SSSXXX. Ejemplo: 2025-04-25T15:05:29.434-05:00.",
   metadata STRUCT<
       partition INT COMMENT "Indica la división lógica y física de un tópico",
       offset BIGINT COMMENT "Indica la posición secuencial de un mensaje dentro de una partición",
@@ -22,7 +22,7 @@ CREATE OR REPLACE TABLE ${env:ENV_CATALOG_NAME_DEVELOP}.bronze.tiendapricessku_b
   p_auditDate DATE COMMENT "Fecha de ejecucion de la rutina de carga al datalake en UTC-0." not null
 )
 CLUSTER BY (auditTime, p_auditDate)
-COMMENT "Esta tabla contiene información detallada sobre los productos, específicamente sobre los SKU (stock keeping units), incluyendo sus precios, costos y márgenes de ganancia. Cada SKU tiene un identificador único y se registran los precios sugeridos, costos base, márgenes de ganancia, precios de referencia, así como cualquier precio fijo que pueda aplicarse. Además, se incluye la fecha y hora de la última actualización de la información del SKU, lo que permite un seguimiento efectivo de los cambios en el catálogo."
+COMMENT "Esta tabla contiene información detallada sobre los productos, específicamente sobre los SKU (stock keeping units). Incluye datos como el identificador único, precios de venta y costo, márgenes de ganancia, así como información sobre actualizaciones y precios fijos. Estos datos son esenciales para la gestión de inventarios y la estrategia de precios en la plataforma VTEX."
  TBLPROPERTIES (
   'delta.logRetentionDuration' = 'interval 30 days',
   'delta.deletedFileRetentionDuration' = 'interval 30 days',
